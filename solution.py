83c75844fe205f9380e76029819bd44999ee2e47
mayerli23 <116215243+mayerli23@users.noreply.github.com>
2022-11-09 20:57:59 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,104 +1,140 @@
-# ============Pregunta 1 (7pts)============
-def desencriptando_mensajes(horas, mensaje_encriptado):
-    mensaje_desencriptado = ""
-  
-    # Codigo para Pregunta 1 comienza aqui
-    msg = mensaje_encriptado.lower()
-    n = len(msg)
-    for i in range(n):
-        cod = ord(msg[i])
-        if cod>=97 and cod<=122:      # letras
-            cod = cod - horas
-            if cod < 97:
-                cod = cod + 26
-            car = chr(cod)
-        elif msg[i]=="$":
-            car = "."
-        elif msg[i]=="&":
-            car = ","
-        elif msg[i]=="/":
-            car = " "
-        mensaje_desencriptado += car
-    # Codigo para Pregunta 1 acaba aqui
-    return mensaje_desencriptado
+import imageio
+import numpy as np
 
-
-# ============Pregunta 2 (8pts)============
-
-def resolviendo_el_mensaje(mensaje_copiado, horas, mensaje_encriptado, R):
-    mensaje_desencriptado = ""
-    mensaje_ordenado = "" 
-    es_copia = False # Usar esta variable para averiguar si es una copia (True) o no (False)
+def leer_imagen(ruta):
+    """
+    La función leer_imagen recibe un string con la ruta
+    de una imagen en formato BMP y retorna una lista de
+    tres dimensiones con el mapa de bits de la imagen.
+    Asimismo, convertimos la lista de numpy a una lista
+    común y corriente.
+    """
+    np_array = np.array(imageio.imread(ruta), dtype='int')
     
-    # Codigo para Pregunta 2 comienza aqui
-    copia = mensaje_copiado.lower()
-    msg = mensaje_encriptado.lower()
-    n = len(msg)
-    for i in range(n):
-        cod = ord(msg[i])
-        if cod>=97 and cod<=122:      # letras
-            cod = cod + horas
-            if cod >122:
-                cod = cod - 26
-            car = chr(cod)
-        elif msg[i]=="$":
-            car = "."
-        elif msg[i]=="&":
-            car = ","
-        elif msg[i]=="/":
-            car = " "
-        mensaje_desencriptado += car
-    # ordenamos
-    lista = mensaje_desencriptado.split()
-    for r in range(R):
-        cad = lista.pop(0)
-        lista.append(cad)
-    mensaje_ordenado = " ".join(lista)        
-    es_copia = copia == mensaje_ordenado
-    # Codigo para Pregunta 2 acaba aqui
+    lista_3d = np_array.tolist()
+    return lista_3d
+
+
+def guardar_imagen(ruta, lista_3d):
+    """
+    La función guardar_imagen recibe una lista de 3
+    dimensiones con el mapa de bits de la imagen
+    y retorna la imagen en formato bmp.
+    """
+    return imageio.imwrite(ruta, np.array(lista_3d, dtype='uint8'))
+
+
+def oscurecer(lista_3d, pct):
+    result = list()
+    # SU SOLUCIÓN EMPIEZA AQUÍ
+
+    for gi in range(len(lista_3d)):
+        fila = []
+        for letra in range(len(lista_3d[gi])):
+            tic=[round(lista_3d[gi][letra][0]*(1-pct)),round(lista_3d[gi][letra][1]*(1-pct)),round(lista_3d[gi][letra][2]*(1-pct))]
+            fila.append(tic)
+        result.append(fila)
+
+    # SU SOLUCIÓN TERMINA AQUÍ
+    return result
+
+
+def difuminado(lista_3d):
+    result = [row.copy() for row in lista_3d]
+    # SU SOLUCIÓN EMPIEZA AQUÍ
+
+    result=[]
+    for gi in range(len(lista_3d)):
+        fila = []
+        for letra in range(len(lista_3d[gi])):
+            if (letra==0):
+                tic=[lista_3d[gi][letra][0],lista_3d[gi][letra][1],lista_3d[gi][letra][2]]
+                fila.append(tic)
+            elif (letra==1):
+                tic=[(lista_3d[gi][letra][0]+lista_3d[gi][letra-1][0])//2,(lista_3d[gi][letra][1]+lista_3d[gi][letra-1][1])//2,(lista_3d[gi][letra][2]+lista_3d[gi][letra-1][2])//2]
+                fila.append(tic)
+            else:
+                tic=[(lista_3d[gi][letra][0]+lista_3d[gi][letra-1][0]+lista_3d[gi][letra-2][0])//3,(lista_3d[gi][letra][1]+lista_3d[gi][letra-1][1]+lista_3d[gi][letra-2][1])//3,(lista_3d[gi][letra][2]+lista_3d[gi][letra-1][2]+lista_3d[gi][letra-2][2])//3]
+                fila.append(tic)
+
+        result.append(fila)
+
+
+    # SU SOLUCIÓN TERMINA AQUÍ
+    return result
+
+
+def cuadrantes(lista_3d, orden):
+    result = list()
+    # SU SOLUCIÓN EMPIEZA AQUÍ
+    larggg=0
+    anchh=0
+    cdrt0=[]
+    cdrt1=[]
+    cdrt2=[]
+    cdrt3=[]
+    for gi in range(len(lista_3d)):
+        larggg+=1
+        for j in range(len(lista_3d[gi])):
+            if(gi>=1):
+                break
+            else:
+                anchh+=1
+
+    for gi in range(larggg//2):
+        fila = []
+        for letra in range(anchh//2):
+            tic=[lista_3d[gi][letra][0],lista_3d[gi][letra][1],lista_3d[gi][letra][2]]
+            fila.append(tic)
+        cdrt0.append(fila)
+
+    for gi in range(larggg//2):
+        fila = []
+        for letra in range(anchh//2,anchh):
+            tic=[lista_3d[gi][letra][0],lista_3d[gi][letra][1],lista_3d[gi][letra][2]]
+            fila.append(tic)
+        cdrt1.append(fila)
+
+    for gi in range(larggg//2,larggg):
+        fila = []
+        for letra in range(anchh//2):
+            tic=[lista_3d[gi][letra][0],lista_3d[gi][letra][1],lista_3d[gi][letra][2]]
+            fila.append(tic)
+        cdrt2.append(fila)
+
+    for gi in range(larggg//2,larggg):
+        fila= []
+        for letra in range(anchh//2, anchh):
+            tic=[lista_3d[gi][letra][0],lista_3d[gi][letra][1],lista_3d[gi][letra][2]]
+            fila.append(tic)
+        cdrt3.append(fila)
+
+    cuadrantes=[cdrt0,cdrt1,cdrt2,cdrt3]
+
     
-    resultado_final = [mensaje_desencriptado, mensaje_ordenado, es_copia]
+    for gi in range(len(cuadrantes[orden[0]])):
+        for letra in range(len(cuadrantes[orden[1]][gi])):
+            cuadrantes[orden[0]][gi].append(cuadrantes[orden[1]][gi][letra])
+
+
+    
+    for gi in range(len(cuadrantes[orden[3]])):
+        for letra in range(len(cuadrantes[orden[3]][gi])):
+            cuadrantes[orden[2]][gi].append(cuadrantes[orden[3]][gi][letra])
+
+
+    
+    for gi in range(len(cuadrantes[orden[2]])):
+        cuadrantes[orden[0]].append(cuadrantes[orden[2]][gi])
+
+    for gi in range(len(cuadrantes[orden[0]])):
+        fila = []
+        for letra in range(len(cuadrantes[orden[0]][gi])):
+            tic=[cuadrantes[orden[0]][gi][letra][0],cuadrantes[orden[0]][gi][letra][1],cuadrantes[orden[0]][gi][letra][2]]
+            fila.append(tic)
+        result.append(fila)
+
+    # SU SOLUCIÓN TERMINA AQUÍ
+    return result
+    
    
-    return resultado_final
-
-# ============Pregunta 3 (5pts)============
-# 4 puntos
-def posibles_sospechosos(equipos, calificaciones):
-    sospechosos = ""
-
-    # Codigo para Pregunta 3 comienza aqui
-    sospechosos = []
-    nombres =  equipos.split(", ")
-    notas =  calificaciones.split(", ")
-    n = len(nombres)
-    for i in range(n):
-        base = notas[i]
-        cont = 0
-        for nota in notas:
-            if int(base) == int(nota):
-                cont += 1
-        if cont > 2:
-            sospechosos.append(nombres[i])
-      
-    # Codigo para Pregunta 3 acaba aqui
-    return sospechosos
-
-# ==== MAIN =====
-msg = desencriptando_mensajes(12, "fuqzqe/cgq/abfuyulmd/qx/oapusa/oaz/puhueuaz/k/oazcguefm$/fqz/ogupmpa/oaz/qx/dqpazpqa/pq/hmxadqe$")
-print(msg)
-
-msg = resolviendo_el_mensaje("Tienes que optimizar el codigo con division y conquista. Ten cuidado con el redondeo de valores.", 
-                             10, "hutedtue/tu/lqbehui$/jyudui/gku/efjycypqh/ub/setywe/sed/tylyiyed/o/sedgkyijq$/jud/skytqte/sed/ub",3)
-print(msg)
-
-msg = resolviendo_el_mensaje("Tienes que optimizar el codigo con division y conquista. Ten cuidado con el redondeo de valores.", 
-                             15, "topl/op/nzxz/cpdzwgpcwz$/yz/epyrz/yt",4)
-print(msg)
-
-
-equipos = "Alfalfa, Rabano, Choclito, Zanahoria, Frijolito, Arroz, Trigo"
-calificaciones = "58, 14, 55, 60, 57, 75, 57"
-
-lista = posibles_sospechosos(equipos, calificaciones)
-print(lista)
-
